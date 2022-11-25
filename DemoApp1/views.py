70379aa9bf50cefb2e80c59997fdd0475ae24747
Vijay95050 <118589648+Vijay95050@users.noreply.github.com>
2022-11-25 12:48:24 +0530

    Update views.py

--- DemoApp1/views.py
+++ DemoApp1/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+anfrom django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def f1(req):
-    return HttpResponse("<h1>Hello from DemoApp1 f1()..</h1><hr />")
+    return HttpResponse("<h1>Hello from DemoApp1 f1().. take care and bye..</h1><hr />")
 def f2(req):
     return HttpResponse("<h1> Hello from DemoApp2 f2().. and Have a nice day</h1><hr />")
