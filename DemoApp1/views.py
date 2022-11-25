4052ead31097eb1bcd09aec2e0b351ee09a2f5ee
Vijay95050 <vijayjagannadam2@gmail.com>
2022-11-25 12:40:07 +0530

    Initial commit

---
+++ DemoApp1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def f1(req):
+    return HttpResponse("<h1>Hello from DemoApp1 f1()..</h1><hr />")
+def f2(req):
+    return HttpResponse("<h1> Hello from DemoApp2 f2()..</h1><hr />")
