f14bbcb8903c9430efb41c2e9ecc8b8f5c985e2f
Vijay95050 <vijayjagannadam2@gmail.com>
2022-11-25 12:45:25 +0530

    second commit

--- DemoApp1/views.py
+++ DemoApp1/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def f1(req):
     return HttpResponse("<h1>Hello from DemoApp1 f1()..</h1><hr />")
 def f2(req):
-    return HttpResponse("<h1> Hello from DemoApp2 f2()..</h1><hr />")
+    return HttpResponse("<h1> Hello from DemoApp2 f2().. and Have a nice day</h1><hr />")
